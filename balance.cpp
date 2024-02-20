2ca0516a3c1bc71752052540df36b439ae3c400c
Cole Reynolds <cpjreynolds@gmail.com>
2024-02-20 18:56:33 -0500

    more tests

--- balance.cpp
+++ balance.cpp
@@ -17,7 +17,14 @@ TEST_CASE("symbols class")
             data1.scramble();
             CHECK_EQ(data1, result1);
         }
-        SUBCASE("n=8") {}
+        SUBCASE("n=8")
+        {
+            symbols data(8);
+            symbols result = {-1, 1, -1, -1, -1, 1, -1, -1, 1,
+                              -1, 1, 1,  -1, 1,  1, 1,  -1};
+            data.scramble();
+            CHECK_EQ(data, result);
+        }
     }
 
     SUBCASE("cut_and_paste")
@@ -29,15 +36,18 @@ TEST_CASE("symbols class")
             data.scramble();
             data.cut_and_splice();
             CHECK_EQ(data, result);
+            CHECK(data.is_balanced());
         }
 
         SUBCASE("n=8")
         {
             symbols data(8);
-            symbols result = {1, 1, -1, -1, 1, -1};
+            symbols result = {1, -1, 1, -1, 1,  1,  -1, 1,
+                              1, 1,  1, -1, -1, -1, -1, -1};
             data.scramble();
             data.cut_and_splice();
             CHECK_EQ(data, result);
+            CHECK(data.is_balanced());
         }
     }
 
